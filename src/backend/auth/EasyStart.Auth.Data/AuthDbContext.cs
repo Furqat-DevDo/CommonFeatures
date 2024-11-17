0c7e5116ab0e5cff1b315c5e6bc73d1454311a4a
Furqat-DevDo <furqata93@gmail.com>
2024-11-17 20:03:59 +0500

    feat : add duende identity server project template

---
+++ src/backend/auth/EasyStart.Auth.Data/AuthDbContext.cs
@@ -0,0 +1,23 @@
+﻿// EasyStart
+// Copyright (C) 2024
+// 
+// This file is licensed under the GNU General Public License v3.0.
+// Redistribution, modification, or use of this file is allowed according to the terms of the License.
+// Full license details: https://www.gnu.org/licenses/gpl-3.0.html.
+// 
+// This project is open source but may transition to a paid license in the future.
+// Please refer to furqata93@gmail.com for updated licensing information.
+
+using EasyStart.Auth.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyStart.Auth.Data;
+
+public class AuthDbContext : IdentityDbContext<IdentityUserExtension,IdentityRoleExtension,Guid>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+        
+    }
+}
---
+++ src/backend/auth/EasyStart.Auth.Data/Entities/IdentityRoleExtension.cs
@@ -0,0 +1,18 @@
+﻿// EasyStart
+// Copyright (C) 2024
+// 
+// This file is licensed under the GNU General Public License v3.0.
+// Redistribution, modification, or use of this file is allowed according to the terms of the License.
+// Full license details: https://www.gnu.org/licenses/gpl-3.0.html.
+// 
+// This project is open source but may transition to a paid license in the future.
+// Please refer to furqata93@gmail.com for updated licensing information.
+
+using Microsoft.AspNetCore.Identity;
+
+namespace EasyStart.Auth.Data.Entities;
+
+public class IdentityRoleExtension : IdentityRole<Guid>
+{
+    
+}
