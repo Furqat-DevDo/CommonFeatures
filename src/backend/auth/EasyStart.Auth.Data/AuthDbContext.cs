83b2e0cbea7958ac81dbd97bd2899b61a056b260
Furqat-DevDo <furqata93@gmail.com>
2024-11-17 20:39:39 +0500

    docs : add editorconfig file and change license info

--- src/backend/auth/EasyStart.Auth.Data/AuthDbContext.cs
+++ src/backend/auth/EasyStart.Auth.Data/AuthDbContext.cs
@@ -1,12 +1,6 @@
-﻿// EasyStart
-// Copyright (C) 2024
-// 
-// This file is licensed under the GNU General Public License v3.0.
-// Redistribution, modification, or use of this file is allowed according to the terms of the License.
-// Full license details: https://www.gnu.org/licenses/gpl-3.0.html.
-// 
-// This project is open source but may transition to a paid license in the future.
-// Please refer to furqata93@gmail.com for updated licensing information.
+﻿// EasyStart - Copyright (C) 2024. GNU GPL v3.0.
+// Terms: https://www.gnu.org/licenses/gpl-3.0.html.
+// Contact: furqata93@gmail.com.
 
 using EasyStart.Auth.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
